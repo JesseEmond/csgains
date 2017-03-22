32c727192ac52de4113a165a6d472c0916061132
JesseEmond <emond.jesse@gmail.com>
2017-03-21 23:52:06 -0400

    Partial update from latest version
    Still missing updated challenge solver with shortest path included.

--- miner/coinslib/BaseClient.py
+++ miner/coinslib/BaseClient.py
@@ -19,6 +19,7 @@ class BaseClient:
         self.public_key = None
         self.private_key = None
         self.wallet_id = ""
+        self.wallet_name = ""
 
     def sign_message(self, digest):
         signer = PKCS1_v1_5.new(self.private_key)
@@ -63,13 +64,9 @@ class BaseClient:
         else:
             self.socket = await websockets.client.connect("ws://{0}:{1}/client".format(self.hostname, self.port))
 
-    async def submit(self, challenge_id, nonce, hash):
-        # signature
-        hasher = SHA256.new()
-        hasher.update("{0},{1},{2}".format(challenge_id, nonce, hash).encode("ascii"))
-        signature = self.sign_message(hasher)
+    async def submit(self, nonce):
 
-        command = {'command': 'submission', 'args': {'challenge_id': challenge_id, 'nonce': nonce, 'hash': hash, 'signature': signature, 'wallet_id': self.wallet_id}}
+        command = {'command': 'submission', 'args': {'nonce': nonce, 'wallet_id': self.wallet_id}}
         message = json.dumps(command)
         await self.socket.send(message)
 
@@ -77,6 +74,17 @@ class BaseClient:
         response = json.loads(message)
 
         print("Submission result: {0}".format(response))
+
+        if 'error' in response:
+            print("Error during submission : {0}".format(response["error"]))
+
+        if 'challenge_name' in response:
+            # we got a new challenge
+            challenge = Challenge()
+            challenge.fill_from_challenge(response)
+
+            return challenge
+        return None
 
     async def get_challenge_solution(self, challenge_id):
         command = {'command': 'get_challenge_solution', 'args': {'challenge_id': challenge_id}}
@@ -124,10 +132,45 @@ class BaseClient:
         await self.socket.send(message)
 
         message = await self.socket.recv()
-        data = json.loads(message)
+        response = json.loads(message)
 
-        if data['response']:
-            return data['id']
+        if 'error' in response:
+            print("Error during create_transaction : {0}".format(response['error']))
+
+        if 'id' in response:
+            return response['id']
 
         return None
 
+    async def register_wallet(self):
+        # create the wallet
+        keyString = ""
+        for b in self.public_key.exportKey(format='PEM'):
+            keyString += chr(b)
+
+        hasher = SHA256.new()
+        hasher.update(self.public_key.exportKey(format='DER'))
+        signature = self.sign_message(hasher)
+
+        command = {"command": "register_wallet", "args": {"name": self.wallet_name, "key": keyString, "signature": signature}}
+        message = json.dumps(command)
+        await self.socket.send(message)
+        message = await self.socket.recv()
+        response = json.loads(message)
+
+        if 'error' in response:
+            print("Error during register_wallet : {0}".format(response['error']))
+
+        if 'wallet_id' in response:
+            print("Wallet created : {}".format(response["wallet_id"]))
+            return
+
+
+    async def get_ca_server_info(self):
+        command = {"command": "ca_server_info", "args": {}}
+        message = json.dumps(command)
+        await self.socket.send(message)
+        message = await self.socket.recv()
+        response = json.loads(message)
+
+        print(response)
--- miner/main.py
+++ miner/main.py
@@ -1,13 +1,22 @@
-import MinerClient
-import asyncio
 import sys
 sys.path.append('..')
 
+import MinerClient
+import asyncio
+
+use_ssl = True
 keys_dir = ""
+host = "cscoins.2017.csgames.org"
 
 if len(sys.argv) > 1:
     keys_dir = sys.argv[1]
 
-mc = MinerClient.MinerClient(keys_dir)
+if len(sys.argv) > 2:
+    host = sys.argv[2]
 
+if len(sys.argv) > 3:
+    use_ssl = sys.argv[3] != "0"
+
+mc = MinerClient.MinerClient(keys_dir, host)
+if not use_ssl: mc.ssl = False
 asyncio.get_event_loop().run_until_complete(mc.client_loop())
