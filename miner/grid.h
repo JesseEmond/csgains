f9dcfcf303b3645ad0024312e19ccd57c3fc7f22
JesseEmond <emond.jesse@gmail.com>
2017-03-22 18:22:32 -0400

    Encode positions as ints

--- miner/grid.h
+++ miner/grid.h
@@ -14,6 +14,7 @@ using Position = std::pair<Coordinate, Coordinate>;
 using Cell = char;
 using Row = std::vector<Cell>;
 using Grid = std::vector<Row>;
+using EncodedPosition = int;
 
 const Cell Empty = '.';
 const Cell Blocker = '#';
@@ -21,25 +22,32 @@ const Cell Start = 's';
 const Cell End = 'e';
 
 using Path = std::vector<Position>;
-using CameFrom = std::vector<std::pair<Position, Position>>;
-using CostSoFar = std::vector<std::pair<Position, int>>;
-using HeapNode = std::pair<int, Position>;
+using CameFrom = std::vector<std::pair<EncodedPosition, EncodedPosition>>;
+using CostSoFar = std::vector<std::pair<EncodedPosition, int>>;
+using HeapNode = std::pair<int, EncodedPosition>;
 
 inline int heuristic(Position a, Position b) {
     // manhattan
     return std::abs(a.first - b.first) + std::abs(a.second - b.second);
 }
 
-Path reconstruct_path(Position start, Position end, const CameFrom& came_from) {
+inline EncodedPosition encode(Position p) {
+    return ((p.first & 0xFFFF) << 16) + (p.second & 0xFFFF);
+}
+inline Position decode(EncodedPosition n) {
+    return Position{(n & 0xFFFF0000) >> 16, n & 0xFFFF};
+}
+
+Path reconstruct_path(EncodedPosition start, EncodedPosition end, const CameFrom& came_from) {
     Path path;
     while (end != start) {
-        path.push_back(end);
-        const auto it = std::find_if(came_from.begin(), came_from.end(), [&end] (const auto& p) {
+        path.push_back(decode(end));
+        const auto it = std::find_if(came_from.begin(), came_from.end(), [&end] (auto p) {
             return p.first == end;
         });
         end = it->second;
     }
-    path.push_back(start);
+    path.push_back(decode(start));
     reverse(path.begin(), path.end());
     return path;
 }
@@ -51,37 +59,41 @@ Path shortest_path(const Grid& grid, Position start, Position end,
                    CameFrom& came_from, CostSoFar& cost_so_far) {
     static const auto frontier_comparator = std::greater_equal<HeapNode>();
     std::vector<HeapNode> frontier;
-    frontier.emplace_back(0, start);
+    const auto encoded_start = encode(start);
+    const auto encoded_end = encode(end);
+    frontier.emplace_back(0, encoded_start);
 
     came_from.clear();
     cost_so_far.clear();
 
-    came_from.emplace_back(start, start);
-    cost_so_far.emplace_back(start, 0);
+    came_from.emplace_back(encoded_start, encoded_start);
+    cost_so_far.emplace_back(encoded_start, 0);
 
     while (!frontier.empty()) {
         std::pop_heap(frontier.begin(), frontier.end(), frontier_comparator);
         const auto pos = frontier.back().second;
         frontier.pop_back();
 
-        if (pos == end) {
-            return reconstruct_path(start, end, came_from);
+        if (pos == encoded_end) {
+            return reconstruct_path(encoded_start, encoded_end, came_from);
         }
 
         for (const auto direction : directions) {
-            const auto next = Position(pos.first + direction.first,
-                                       pos.second + direction.second);
-            if (next.first <= 0 ||
-                static_cast<unsigned int>(next.first+1) >= grid.size() ||
-                next.second <= 0 ||
-                static_cast<unsigned int>(next.second+1) >= grid.size() ||
-                grid[next.first][next.second] == Blocker) continue;
+            const auto decoded = decode(pos);
+            const auto next_pos = Position(decoded.first + direction.first,
+                                           decoded.second + direction.second);
+            if (next_pos.first <= 0 ||
+                static_cast<unsigned int>(next_pos.first+1) >= grid.size() ||
+                next_pos.second <= 0 ||
+                static_cast<unsigned int>(next_pos.second+1) >= grid.size() ||
+                grid[next_pos.first][next_pos.second] == Blocker) continue;
+            const auto next = encode(next_pos);
 
-            auto curItCost = std::find_if(cost_so_far.begin(), cost_so_far.end(), [&pos](const auto& cost) {
+            auto curItCost = std::find_if(cost_so_far.begin(), cost_so_far.end(), [&pos](auto cost) {
                 return cost.first == pos;
             });
             const auto new_cost = curItCost->second + 1;
-            auto newIt = std::find_if(cost_so_far.begin(), cost_so_far.end(), [&next](const auto& cost) {
+            auto newIt = std::find_if(cost_so_far.begin(), cost_so_far.end(), [&next](auto cost) {
                 return cost.first == next;
             });
             if (newIt == cost_so_far.end()) {
@@ -92,7 +104,7 @@ Path shortest_path(const Grid& grid, Position start, Position end,
                 newIt->second = new_cost;
                 const auto priority = new_cost +
                                       (use_heuristic ?
-                                       heuristic(next, end) : 0);
+                                       heuristic(next_pos, end) : 0);
                 frontier.emplace_back(priority, next);
                 std::push_heap(frontier.begin(), frontier.end(), frontier_comparator);
                 came_from.emplace_back(next, pos);
