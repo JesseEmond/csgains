685573ba60019ad116f92e6cf6e8c481b6a61024
JesseEmond <emond.jesse@gmail.com>
2017-03-24 20:49:43 -0400

    Safety checks

--- miner/cpp_solvers.py
+++ miner/cpp_solvers.py
@@ -7,19 +7,28 @@ lib.solve_shortest_path.restype = ctypes.c_uint64
 lib.unit_test()
 
 def solve_sorted_list(target_prefix, previous_hash, nb_elements):
-    return lib.solve_list_sort(target_prefix.encode('ascii'),
-                               previous_hash.encode('ascii'),
-                               nb_elements,
-                               True)
+    try:
+        return lib.solve_list_sort(target_prefix.encode('ascii'),
+                                   previous_hash.encode('ascii'),
+                                   nb_elements,
+                                   True)
+    except:
+        return 0
 
 def solve_reverse_sorted_list(target_prefix, previous_hash, nb_elements):
-    return lib.solve_list_sort(target_prefix.encode('ascii'),
-                               previous_hash.encode('ascii'),
-                               nb_elements,
-                               False)
+    try:
+        return lib.solve_list_sort(target_prefix.encode('ascii'),
+                                   previous_hash.encode('ascii'),
+                                   nb_elements,
+                                   False)
+    except:
+        return 0
 
 def solve_shortest_path(target_prefix, previous_hash, nb_blockers, grid_size):
-    return lib.solve_shortest_path(target_prefix.encode('ascii'),
-                                   previous_hash.encode('ascii'),
-                                   nb_blockers,
-                                   grid_size)
+    try:
+        return lib.solve_shortest_path(target_prefix.encode('ascii'),
+                                       previous_hash.encode('ascii'),
+                                       nb_blockers,
+                                       grid_size)
+    except:
+        return 0
